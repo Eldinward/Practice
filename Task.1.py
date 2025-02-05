7c4579402e6937492af6583eb9da88b83b934426
Eldinward <shulgamatveymacs@gmail.com>
2025-02-05 17:53:27 +0200

    Update Task.1.py

--- Task.1.py
+++ Task.1.py
@@ -1,1 +1,12 @@
+def process_string(s: str) -> tuple:
+    vowels = "аеёиоуыэюяaeiou"
+    consonants = "бвгджзйклмнпрстфхцчшщbcdfghjklmnpqrstvwxyz"
+    
+    vowels_in_s = sorted([ch for ch in s.lower() if ch in vowels])
+    consonants_in_s = sorted([ch for ch in s.lower() if ch in consonants])
+    
+    return ("".join(vowels_in_s), len(vowels_in_s), "".join(consonants_in_s))
 
+input_str = "Привіт, як справи?"
+result = process_string(input_str)
+print(result)

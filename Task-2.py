704fca3c1a6952711f53b69d053b68cf081778cc
Eldinward <shulgamatveymacs@gmail.com>
2025-02-05 18:40:36 +0200

    Update Task-2.py

--- Task-2.py
+++ Task-2.py
@@ -1,1 +1,8 @@
+# Завдання перше:
+n = int(input("Введіть число n: "))
 
+for a in range(1, n + 1):
+    for b in range(a, n + 1):
+        for c in range(b, n + 1):
+            if a * a + b * b == c * c:
+                print(a, b, c)

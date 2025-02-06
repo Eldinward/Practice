0fb99657a2d9f966573213a5635f64a8dafc52d8
Eldinward <shulgamatveymacs@gmail.com>
2025-02-06 10:00:49 +0200

    Update Task-1.py

--- Task-1.py
+++ Task-1.py
@@ -1,12 +1,20 @@
-def process_string(s: str) -> tuple:
-    vowels = "аеёиоуыэюяaeiou"
-    consonants = "бвгджзйклмнпрстфхцчшщbcdfghjklmnpqrstvwxyz"
+def analyze_string(input_string):
+    vowels = "аеєєиіїїоуюяaeiouy"
+    consonants = ""
+    vowels_in_string = ""
+    count_vowels = 0
+    for char in input_string:
+        if char.lower() in vowels:
+            vowels_in_string += char.lower()
+            count_vowels += 1
+        elif char.isalpha():
+            consonants += char.lower()
     
-    vowels_in_s = sorted([ch for ch in s.lower() if ch in vowels])
-    consonants_in_s = sorted([ch for ch in s.lower() if ch in consonants])
+    vowels_in_string = ''.join(sorted(vowels_in_string))
+    consonants = ''.join(sorted(consonants))
     
-    return ("".join(vowels_in_s), len(vowels_in_s), "".join(consonants_in_s))
+    return (vowels_in_string, count_vowels, consonants)
 
-input_str = "Привіт, як справи?"
-result = process_string(input_str)
+input_string = input("Введіть строку: ")
+result = analyze_string(input_string)
 print(result)

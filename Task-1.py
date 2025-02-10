30523ec92474af82933a7793e8cdbfc8a95b9c92
Eldinward <shulgamatveymacs@gmail.com>
2025-02-11 01:27:53 +0200

    Update Task-1.py

--- Task-1.py
+++ Task-1.py
@@ -1,17 +1,19 @@
 def analyze_string(input_string):
-    vowels = "аеєиіїоуюяaeiouy"
-    vowels_in_string = []
-    consonants = []
+    vowels = "аеєєиіїїоуюяaeiouy"
+    consonants = ""
+    vowels_in_string = ""
     count_vowels = 0
-
     for char in input_string:
         if char.lower() in vowels:
-            vowels_in_string.append(char.lower())
+            vowels_in_string += char.lower()
             count_vowels += 1
         elif char.isalpha():
-            consonants.append(char.lower())
-
-    return (''.join(sorted(vowels_in_string)), count_vowels, ''.join(sorted(consonants)))
+            consonants += char.lower()
+    
+    vowels_in_string = ''.join(sorted(vowels_in_string))
+    consonants = ''.join(sorted(consonants))
+    
+    return (vowels_in_string, count_vowels, consonants)
 
 input_string = input("Введіть строку: ")
 result = analyze_string(input_string)
